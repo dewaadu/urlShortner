6d037db266acd4dc5ebb23008c16d19df5d34025
dewaadu <dawitadane7@gmail.com>
2020-12-06 10:43:35 +0300

    url completed

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
+//selecting button to apply onclick and shorten functionality
 document.querySelector(".btn-url").addEventListener("click", shortenUrl);
     
 function shortenUrl(url) {
-
-
+//getting the value in the input field to be shorten
+  var url=document.getElementById("input-field").value;
+  //applying shortening if there is a valid input value only
+if(url){
     document.querySelector(".url-result").style.display="block";
-    var url=document.getElementById("input-field").value;
-
+   
     console.log(url);
 
   var beg="http://dev.ly/";
@@ -15,20 +17,29 @@ function shortenUrl(url) {
         newUrl=newUrl+url[index];
         
     }
+    //calling hash function and saving the value and making it valid
     var hash=stringToHash(newUrl);
     if(hash<0){
         hash=(hash*hash)/100000000000000; 
          hash=Math.round(hash);
       }
+      //intergeting the hash value with the brand
     var result= beg+hash;
+    // targetting html and applying some css
     document.getElementById("prev-url").value=url;
     document.getElementById("new-url").value=result;
     document.getElementById("new-url").style.color="turquoise";
     document.getElementById("new-url").style.fontSize="20px";
     document.getElementById("prev-url").style.fontSize="20px";
   
-    
-     
+}else
+//if the value in the input-field was not valid this warning message occurs
+{
+    document.querySelector(".validity").style.display="block";
+    document.getElementById("input-field").style.borderColor="red";
+    document.getElementById("input-field").style.borderWidth="4px";
+}
+     //function to apply the copy method on the copy button
     function copy() {
         
         var copyText=document.querySelector("#new-url");
@@ -39,7 +50,9 @@ function shortenUrl(url) {
     
     }
     document.querySelector("#copy").addEventListener("click", copy);
+
 }
+//function to generate hash value based on the string in the input-field
 function stringToHash(string) {
     var hash=0;
     if (string.length==0)return hash;
